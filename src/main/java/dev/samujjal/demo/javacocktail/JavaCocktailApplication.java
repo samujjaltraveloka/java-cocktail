3e7f71f0de32a6dd10e64880f8c0897971be3c5e
Samujjal Majumder <samujjal.majumder@traveloka.com>
2019-09-29 09:50:50 +0530

    Generated Spring source

---
+++ src/main/java/dev/samujjal/demo/javacocktail/JavaCocktailApplication.java
@@ -0,0 +1,13 @@
+package dev.samujjal.demo.javacocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCocktailApplication.class, args);
+	}
+
+}
